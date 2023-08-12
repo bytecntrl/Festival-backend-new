066bdaee67d65d95c7435951236302a90db09b35
ByteCntrl <bytecntrl@gmail.com>
2023-08-13 01:09:29 +0200

    Update

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,34 +1,20 @@
 from asyncio import Lock
 
-from pydantic import BaseSettings
-
-LIST_ENV = [
-    "DB_USERNAME",
-    "PASSWORD",
-    "HOST",
-    "PORT",
-    "DB_NAME",
-    "JWT_SECRET",
-    "JWT_TOKEN_EXPIRES",
-]
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
-class Config(BaseSettings):
+class Config(BaseSettings, case_sensitive=True):
     # db
-    DB_USERNAME: str
-    PASSWORD: str
-    HOST: str
-    PORT: str = "5432"
-    DB_NAME: str
+    DB_USERNAME: str = Field(alias="DB_USERNAME")
+    PASSWORD: str = Field(alias="DB_PASSWORD")
+    HOST: str = Field(alias="DB_HOST")
+    PORT: str = Field("5432", alias="DB_PORT")
+    DB_NAME: str = Field(alias="DB_NAME")
 
     # token jwt
-    JWT_SECRET: str
-    JWT_TOKEN_EXPIRES: int
+    JWT_SECRET: str = Field(alias="JWT_SECRET")
+    JWT_TOKEN_EXPIRES: int = Field(alias="JWT_TOKEN_EXPIRES")
 
     # look
-    LOCK = Lock()
-
-    class Config:
-        case_sensitive = True
-
-        fields = {x: {"env": x} for x in LIST_ENV}
+    LOCK: Lock = Lock()
